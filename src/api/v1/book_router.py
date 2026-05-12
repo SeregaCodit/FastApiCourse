3d69ff2b4774d4f4a71d1a59c5dbab0a3978e0bb
SeregaCodit <serega.codit@gmail.com>
2026-05-12 23:52:17 +0300

    refactor(api): add summary and descriptions to routes

--- src/api/v1/book_router.py
+++ src/api/v1/book_router.py
@@ -1,3 +1,5 @@
+from typing import List
+
 from fastapi import APIRouter, HTTPException
 
 from src.core.entities.book import Book
@@ -12,12 +14,30 @@ router = APIRouter(prefix="/book", tags=["books"])
 create_book_use_case = CreateBookUseCase(book_storage)
 get_all_books_use_case = GetAllBooksUseCase(book_storage)
 
-@router.post("/", tags=["books"])
+
+@router.post("/",
+             tags=["books"],
+             summary="Add a new book",
+             description="Create a new book with the given title and author.",
+             )
 def create_book(request: BookCreateRequest) -> Book:
     return create_book_use_case.execute(request.title, request.author)
 
 
-@router.get("/{book_id}", tags=["books"])
+@router.get("/books",
+            tags=["books"],
+            summary="Get all books",
+            description="Retrieve a list of all books in the system.",
+            )
+def get_all_books() -> List[Book]:
+    return get_all_books_use_case.execute()
+
+
+@router.get("/{book_id}",
+            tags=["books"],
+            summary="Get a book by ID",
+            description="Retrieve a book by its unique ID."
+            )
 def get_book(book_id: int) -> Book:
     all_books = book_storage.get_all()
 
--- src/main.py
+++ src/main.py
@@ -8,21 +8,17 @@ from src.infrastructure.repositories.book_repository import book_storage
 from src.core.entities.book import Book
 
 
-# 1. Створюємо функцію lifespan
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    # --- STARTUP: Логіка при запуску ---
-    # Наповнюємо наше сховище початковими даними
+    # --- STARTUP: launch logic ---
+    # add books to storage
     book_storage.save(Book(title="The Great Gatsby", author="F. Scott Fitzgerald"))
     book_storage.save(Book(title="To Kill a Mockingbird", author="Harper Lee"))
 
-    print("Додаток запущено, дані завантажено в пам'ять.")
+    yield  # app works
 
-    yield  # Тут додаток працює і обробляє запити
-
-    # --- SHUTDOWN: Логіка при зупинці ---
-    # Тут можна закривати з'єднання з БД або чистити кеш
-    print("Додаток зупиняється...")
+    # --- SHUTDOWN: shutdown logic ---
+    # close DB connection etc
 
 app = FastAPI(
         title="Clean Architecture API",
@@ -31,7 +27,6 @@ app = FastAPI(
 app.include_router(book_router, prefix="/api/v1")
 
 def main():
-
     uvicorn.run("main:app", reload=True)
 
 
